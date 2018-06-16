ca514ea513dee312953f6c5320254c9b9a75bf1f
Helge Walter <helge@bewalticus.de>
2018-06-16 08:56:55 +0200

    initial commit

---
+++ src/main/java/de/bewalt/spring/shellndata/ShellNDataApplication.java
@@ -0,0 +1,12 @@
+package de.bewalt.spring.shellndata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellNDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellNDataApplication.class, args);
+	}
+}
